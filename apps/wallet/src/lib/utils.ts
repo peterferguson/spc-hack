5ba7dc55420824b0b834cdda41f206f8c2b64f00
Peter Ferguson <peterferguson95@gmail.com>
2024-03-15 23:18:22 +0000

    lint and add username form on wallet

--- apps/wallet/src/lib/utils.ts
+++ apps/wallet/src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+const getPayeeDomain = () => {
+	const isDev = import.meta.env.DEV;
+	const domain = isDev ? "loca.lt" : "web.app";
+	const payeeOrigin = `https://spc-dapp.${domain}`;
+
+	return payeeOrigin;
+};
+
+export const sendMessage = (message: string) => {
+	window.parent.postMessage({ message }, getPayeeDomain());
+};
