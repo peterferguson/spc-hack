d21765edf781c5cc66de69873de25afb299f37ec
Peter Ferguson <peterferguson95@gmail.com>
2024-03-16 00:17:37 +0000

    wip adding credential creation from iframe

--- apps/dapp/src/lib/utils.ts
+++ apps/dapp/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const getPaymentOrigin = () => {
+  const isDev = import.meta.env.DEV;
+  const domain = isDev ? "loca.lt" : "web.app";
+  const paymentOrigin = `https://spc-wallet.${domain}`;
+
+  return paymentOrigin
+}
+
--- apps/wallet/src/lib/utils.ts
+++ apps/wallet/src/lib/utils.ts
@@ -1,11 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import base64url from "base64url";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-const getPayeeDomain = () => {
+export const getPayeeDomain = () => {
 	const isDev = import.meta.env.DEV;
 	const domain = isDev ? "loca.lt" : "web.app";
 	const payeeOrigin = `https://spc-dapp.${domain}`;
@@ -15,4 +16,53 @@ const getPayeeDomain = () => {
 
 export const sendMessage = (message: string) => {
 	window.parent.postMessage({ message }, getPayeeDomain());
+};
+
+
+export const registerSpcCredential = async ({ userId, challenge }: { userId: string, challenge: string }) => {
+  const opts = {
+    attestation: 'none',
+    authenticatorSelection: {
+      authenticatorAttachment: 'platform',
+      userVerification: 'required',
+      residentKey: 'preferred'
+    }
+  } as const satisfies Partial<PublicKeyCredentialCreationOptions>;
+
+  // const options = await _fetch('/auth/registerRequest', opts);
+  const publicKey: Partial<PublicKeyCredentialCreationOptions> = opts
+
+  publicKey.user.id = base64url.decode(userId);
+  publicKey.challenge = base64url.decode(challenge);
+
+  if (publicKey.excludeCredentials) {
+    for (let cred of publicKey.excludeCredentials) {
+      cred.id = base64url.decode(cred.id);
+    }
+  }
+
+  const cred = await navigator.credentials.create({ publicKey });
+
+  if (!cred) throw new Error('No credential returned');
+
+  const credential = {
+    id: cred.id,
+    // rawId: base64url.encode(cred.rawId),
+    type: cred.type,
+  };
+
+  if (cred.response) {
+    const clientDataJSON =
+      base64url.encode(cred.response.clientDataJSON);
+    const attestationObject =
+      base64url.encode(cred.response.attestationObject);
+    credential.response = {
+      clientDataJSON,
+      attestationObject,
+    };
+  }
+
+  localStorage.setItem(`credId`, credential.id);
+
+  // return await _fetch('/auth/registerResponse', credential);
 };
