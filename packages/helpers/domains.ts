5861769f871e16512adf90c7f3a99c083a605411
Peter Ferguson <peterferguson95@gmail.com>
2024-03-16 16:50:27 +0000

    update wallet origin

--- packages/helpers/domains.ts
+++ packages/helpers/domains.ts
@@ -6,7 +6,7 @@ export const getDappOrigin = ({ isDev }: { isDev: boolean }) => {
 };
 
 export const getWalletOrigin = ({ isDev }: { isDev: boolean }) => {
-	const domain = isDev ? "loca.lt" : "web.app";
+	const domain = isDev ? "loca.lt" : "vercel.app"; // firebase no longer works "web.app";
 	const origin = `https://spc-wallet.${domain}`;
 
 	return origin;
