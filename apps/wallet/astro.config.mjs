f9e8c5d324b838a68eaaff99483a27f107571c34
Peter Ferguson <peterferguson95@gmail.com>
2024-03-16 16:30:44 +0000

    add ssr to wallet

--- apps/wallet/astro.config.mjs
+++ apps/wallet/astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind({ applyBaseStyles: false }), react()],
+	output: "server",
+	integrations: [
+		db(),
+		node({ mode: "middleware" }),
+		react(),
+		tailwind({ applyBaseStyles: false }),
+	],
 });
