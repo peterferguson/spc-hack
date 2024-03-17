3df4e8f491e2d2be04f9a473230ec435037b999a
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-03-17 00:35:22 +0000

    wip: adding nft calls to ui

--- apps/dapp/src/components/nft-image-card.tsx
+++ apps/dapp/src/components/nft-image-card.tsx
@@ -1,26 +1,34 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { getNftUri } from "@/lib/example-nft";
+import { getNftUri, getBalance } from "@/lib/example-nft";
 import React from "react";
 
 export function NftImageCard() {
 	const tokenId = 1n;
 	const [uri, setUri] = React.useState<string | null>(null);
+	const [hasClaimed, setHasClaimed] = React.useState(false);
 
 	React.useEffect(() => {
-		getNftUri(tokenId).then((r) => setUri(r));
-	}, [tokenId]);
+		getNftUri(0).then((r) => setUri(r));
+	}, []);
+
+	React.useEffect(() => {
+		getBalance('0x3896a2938d345d3A351cE152AF1b8Cb17bb006be', 0n).then((r) => {
+			console.log({ r });
+			setHasClaimed(r > 0)
+		});
+	}, []);
 
 	return (
 		<Card>
 			<CardHeader>
-				<CardTitle>Card Title</CardTitle>
-				<CardDescription>Card Description</CardDescription>
+				<CardTitle>10 Eth Coupon</CardTitle>
+				<CardDescription>Claim your free 10 eth coupon which can</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<p>Card Content: {uri}</p>
+				<img src={uri} alt="NFT" />
 			</CardContent>
 			<CardFooter>
-				<p>Card Footer</p>
+				<p>You have {hasClaimed ? '' : 'not'} claimed</p>
 			</CardFooter>
 		</Card>
 	);
--- apps/dapp/src/lib/example-nft.ts
+++ apps/dapp/src/lib/example-nft.ts
@@ -1,4 +1,4 @@
-import { encodeFunctionData } from "viem"
+import { encodeFunctionData, type Address } from "viem"
 import { walletClient, publicClient } from './permissionless'
 import { formatExecutionCalldata } from "./safe-account"
 
@@ -12,9 +12,11 @@ const NFT_ADDRESS = '0x4A56fD1D63D99978FDb3aC5C152ea122514b6792'
 const EXAMPLE_NFT_ABI = [
     { "inputs": [], "name": "exchangeForNft", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
     { "inputs": [], "name": "mintCoupon", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-    { "inputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }], "name": "uri", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }]
+    { "inputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }], "name": "uri", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" },
+    { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "uint256", "name": "id", "type": "uint256" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }
+]
 
-// nft function formatting --------------------------------------------
+// nft functions --------------------------------------------
 export const getNftUri = async (tokenId: bigint) => await publicClient.readContract({
     address: NFT_ADDRESS,
     abi: EXAMPLE_NFT_ABI,
@@ -22,6 +24,20 @@ export const getNftUri = async (tokenId: bigint) => await publicClient.readContr
     args: [tokenId],
 })
 
+export const getBalance = async (address: Address, tokenId: bigint) => {
+    if (!address) return 0
+    const results = await publicClient.readContract(
+        {
+            address: NFT_ADDRESS,
+            abi: EXAMPLE_NFT_ABI,
+            functionName: 'balanceOf',
+            args: [address, tokenId],
+        }
+    )
+    return results
+}
+
+// nft function formatting --------------------------------------------
 /// @dev To be called from the Onit 4337 account, we encode our function call into an executeUserOp payload
 export const formatMintCouponToModuleExecutionCalldata = () => formatExecutionCalldata(
     NFT_ADDRESS,
