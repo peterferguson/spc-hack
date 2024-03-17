9b7ff69a1c3f5d075e71b30570f46753058111e4
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-03-17 00:08:52 +0000

    chore: add more formatting fns

--- apps/dapp/src/lib/example-nft.ts
+++ apps/dapp/src/lib/example-nft.ts
@@ -1,8 +1,12 @@
 import { encodeFunctionData } from "viem"
 import { walletClient, publicClient } from './permissionless'
+import { formatExecutionCalldata } from "./safe-account"
 
-// abis --------------------------------------------
+/**
+ * @dev Example NFT contract functions
+ */
 
+// addresses & abis --------------------------------------------
 const NFT_ADDRESS = '0x4A56fD1D63D99978FDb3aC5C152ea122514b6792'
 
 const EXAMPLE_NFT_ABI = [
@@ -10,14 +14,37 @@ const EXAMPLE_NFT_ABI = [
     { "inputs": [], "name": "mintCoupon", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
     { "inputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }], "name": "uri", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }]
 
-// nft calls --------------------------------------------
-
+// nft function formatting --------------------------------------------
 export const getNftUri = async (tokenId: bigint) => await publicClient.readContract({
     address: NFT_ADDRESS,
     abi: EXAMPLE_NFT_ABI,
     functionName: 'uri',
     args: [tokenId],
 })
+
+/// @dev To be called from the Onit 4337 account, we encode our function call into an executeUserOp payload
+export const formatMintCouponToModuleExecutionCalldata = () => formatExecutionCalldata(
+    NFT_ADDRESS,
+    0n,
+    encodeFunctionData({
+        abi: EXAMPLE_NFT_ABI,
+        functionName: 'mintCoupon',
+        args: [],
+    }),
+    0
+)
+
+/// @dev To be called from the Onit 4337 account, we encode our function call into an executeUserOp payload
+export const formatExchangeForNftToModuleExecutionCalldata = () => formatExecutionCalldata(
+    NFT_ADDRESS,
+    0n,
+    encodeFunctionData({
+        abi: EXAMPLE_NFT_ABI,
+        functionName: 'exchangeForNft',
+        args: [],
+    }),
+    0
+)
 
 
 
--- apps/dapp/src/lib/safe-account.ts
+++ apps/dapp/src/lib/safe-account.ts
@@ -1,20 +1,21 @@
 import { encodeFunctionData, type Address, type Hex } from "viem"
 import { walletClient, publicClient } from './permissionless'
 
-// abis --------------------------------------------
+/**
+ * @dev Onit Safe 4337 Module functions
+ */
 
+// abis --------------------------------------------
 const ONIT_FACTORY_ADDRESS = '0x42ab880ea77fc7a09eb6ba0fe82fbc9901c114b6'
 
 const ONIT_FACTORY_ABI = [
     { "type": "function", "name": "createSafe4337", "inputs": [{ "name": "passkeyPublicKey", "type": "uint256[2]", "internalType": "uint256[2]" }, { "name": "nonce", "type": "uint256", "internalType": "uint256" }], "outputs": [{ "name": "onitAccountAddress", "type": "address", "internalType": "address" }], "stateMutability": "nonpayable" }
 ]
-
 const ONIT_SAFE_4337_ABI = [
     { "type": "function", "name": "executeUserOp", "inputs": [{ "name": "to", "type": "address", "internalType": "address" }, { "name": "value", "type": "uint256", "internalType": "uint256" }, { "name": "data", "type": "bytes", "internalType": "bytes" }, { "name": "operation", "type": "uint8", "internalType": "uint8" }], "outputs": [], "stateMutability": "nonpayable" }
 ]
 
 // safe factory calls --------------------------------------------
-
 export const createSafe4337Account = async () => {
     const { request } = await publicClient.simulateContract({
         account: walletClient.account,
@@ -32,10 +33,7 @@ export const createSafe4337Account = async () => {
 }
 
 // 4337 module calls --------------------------------------------
-
-/**
- * @dev Build payload which the entryPoint will call on the sender Onit 4337 account
- */
+/// @dev Build payload which the entryPoint will call on the sender Onit 4337 account
 export const formatExecutionCalldata = (to: Address, value: bigint, data: Hex, operation: number) => encodeFunctionData({
     abi: ONIT_SAFE_4337_ABI,
     functionName: 'executeUserOp',
